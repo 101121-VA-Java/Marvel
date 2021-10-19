895e7d8f136376447afa485e99057c3c9619f628
Sam KC <contact@kcsam.net>
2021-10-19 18:03:47 -0400

    Created Task Project

---
+++ task/src/main/java/com/revature/task.java
@@ -0,0 +1,21 @@
+package com.revature;
+
+public class task {
+	
+	
+	
+//	Create Task.class
+//	- String name
+//	- LocalDate dueDate
+//	- encapsulate the class
+//	- create setter and getter
+//		- manually write at least one of them
+//	- give a toString() and a .equals() method
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("testing");	
+		
+	}
+
+}
