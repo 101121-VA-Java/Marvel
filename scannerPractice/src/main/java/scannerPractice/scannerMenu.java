c5a93ee351773a5b936a3e07898f74bc3c8da147
pkorrie <patiencekorrie@gmail.com>
2021-10-19 15:57:24 -0400

    random method

--- scannerPractice/src/main/java/scannerPractice/scannerMenu.java
+++ scannerPractice/src/main/java/scannerPractice/scannerMenu.java
@@ -1,5 +1,18 @@
 package scannerPractice;
 
-public class scannerMenu {
+//import java.lang.Math; 
+import java.util.Random;
 
+public class scannerMenu {
+	public static void main(String[] args) {
+		randNum();
+		
+	}
+	
+	public static void randNum(){
+		Random random = new Random();
+		int y = random.nextInt(1000);  
+//		System.out.println("Random Number is : " + Math.random());
+		System.out.println("Random Number is : " + y);
+		}
 }
