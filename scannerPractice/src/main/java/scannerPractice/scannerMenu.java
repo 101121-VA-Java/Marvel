8970f6637ec159321044a36206212a3aac8b6869
pkorrie <patiencekorrie@gmail.com>
2021-10-19 16:05:16 -0400

    added a different way to random

--- scannerPractice/src/main/java/scannerPractice/scannerMenu.java
+++ scannerPractice/src/main/java/scannerPractice/scannerMenu.java
@@ -1,6 +1,6 @@
 package scannerPractice;
 
-//import java.lang.Math; 
+import java.lang.Math; 
 import java.util.Random;
 
 public class scannerMenu {
@@ -11,8 +11,8 @@ public class scannerMenu {
 	
 	public static void randNum(){
 		Random random = new Random();
-		int y = random.nextInt(1000);  
-//		System.out.println("Random Number is : " + Math.random());
-		System.out.println("Random Number is : " + y);
+//		int y = random.nextInt(1000);  
+		System.out.println("Random Number is : " + Math.round(Math.random()*100));
+//		System.out.println("Random Number is : " + y);
 		}
 }
